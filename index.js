4d8bf2037b49200607b3692bcfa29ec0266a0dfd
Rob Phoenix <rob.phoenix@bt.com>
2016-04-05 11:27:03 +0100

    add start again button

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const remote = require('electron').remote;
 const dialog = remote.require('dialog');
 var Firebase = require("firebase");
 
+var projectName = "";
+
 var loginPageContinueButton = document.getElementById('loginPageContinue');
 loginPageContinueButton.addEventListener('click', function(event) {
   var btEmailInputValue = document.getElementById('btEmailInput').value;
@@ -17,6 +19,10 @@ loginPageContinueButton.addEventListener('click', function(event) {
     document.getElementById('alertMessage').innerHTML = "Please Enter a Project name.";
     document.getElementById('alertMessage').style.visibility = "visible";
   } else {
+    projectName = document.getElementById('projectNameInput').value;
+    var d = new Date()
+    var defaultFilename = projectName.replace(/ /g,"_") + "_inventory_" + d.getFullYear() + (d.getMonth() + 1) + d.getDate() + d.getHours() + d.getMinutes() + d.getSeconds();
+    document.getElementById('inventoryFilename').value = defaultFilename;
     document.getElementById('alertMessage').style.visibility = "hidden";
     document.getElementById('loginPage').style.display = "none";
     document.getElementById('inputForm').style.display = "block";
@@ -24,9 +30,7 @@ loginPageContinueButton.addEventListener('click', function(event) {
 });
 
 
-var d = new Date()
-var defaultFilename = "Inventory_" + d.getFullYear() + "_" + (d.getMonth() + 1) + "_" + d.getDate() + "_" + d.getHours() + d.getMinutes() + d.getSeconds();
-document.getElementById('inventoryFilename').value = defaultFilename;
+
 
 var showFilesDirButton = document.getElementById('showFilesDirSelect');
 var outputDirButton = document.getElementById('outputDirSelect');
@@ -48,9 +52,9 @@ var buildButton = document.getElementById('build');
 buildButton.addEventListener('click', function (event) {
   var showFilesDirPath = document.getElementById('showFilesDirPath').value;
   var outputDirPath = document.getElementById('outputDirPath').value;
-  var inventoryFilename = document.getElementById('inventoryFilename').value;
-  var inputForm = document.getElementById('inputForm');
-  inputForm.parentNode.removeChild(inputForm);
+  var inventoryFilename = document.getElementById('inventoryFilename').value.replace(/ /g,"_");
+  document.getElementById('inputForm').style.display = "none";
+  // inputForm.parentNode.removeChild(inputForm);
   ipcRenderer.send('build', showFilesDirPath, outputDirPath, inventoryFilename);
 });
 
@@ -58,4 +62,15 @@ ipcRenderer.on('result', function (event, result, inventoryFilename) {
     document.getElementById('resultMessage').style.display = "block";
     document.getElementById('msg').innerHTML = inventoryFilename + ".csv";
     document.getElementById('resultCSVData').innerHTML = result;
+});
+
+var startAgainButton = document.getElementById('startAgain');
+
+startAgainButton.addEventListener('click', function (event) {
+  console.log("pressed");
+  document.getElementById('alertMessage').style.visibility = "hidden";
+  document.getElementById('resultMessage').style.display = "none";
+  document.getElementById('showFilesDirPath').value = "";
+  document.getElementById('outputDirPath').value = "";
+  document.getElementById('loginPage').style.display = "block";
 });
